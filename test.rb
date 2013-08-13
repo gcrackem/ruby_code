b4f030ce4aa4fd17fe4de11b22d545050d2ec4ab
root <root@localhost.localdomain>
2013-08-13 18:37:08 -0400

    commented out stuff that doesn't work...

--- test.rb
+++ test.rb
@@ -4,8 +4,8 @@ require 'msf/core'
 require 'rex'
 require 'msf/core/post/windows/registry'
 
-@client = client
-rhost = Rex::Socket.source_address("1.2.3.4")
+#@client = client
+#rhost = Rex::Socket.source_address("1.2.3.4")
 
 puts "Enter the location of your payload [example:] /root/payload.exe"
 payload = gets.chomp
@@ -14,8 +14,9 @@ dir = gets.chomp
 
 if payload != nil
 	puts "Uploading payload from #{payload} to Windows folder #{dir}"
-	write=::REX::console.write(testing)
-	return write
+	puts "#{rhost}"
+	#write=REX::console.write(testing)
+	#return write
 else
 	puts "Error! No payload selected"
 end
