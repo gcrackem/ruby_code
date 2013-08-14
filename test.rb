a9dbb8d28ce0cd8e7f100039264254269d33f10e
gcrackem <emailchase@gmail.com>
2013-08-13 21:56:08 -0500

    updated error output

--- test.rb
+++ test.rb
@@ -6,11 +6,11 @@ payload = gets.chomp
 puts "Enter the directory to upload our payload on the windows machine [example:] c:\\windows\\system32"
 dir = gets.chomp
 
-if payload != nil
+if payload != nil || dir != nil
 	puts "Uploading payload from #{payload} to Windows folder #{dir}"
 	console.run_single("upload #{payload} #{dir}")
 	#write=REX::console.write(testing)
 	#return write
 else
-	puts "Error! No payload selected"
+	puts "Error: Need both payload location and target directory"
 end
